9f9d332af056e5f30d51311da647c55ab9e0bef3
g12950sn <g12950sn@m20209.tsuda.ac.jp>
2014-04-29 15:51:55 +0900

    テスト製作、エラー修正

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -1,8 +1,9 @@
 package snakecamel;
 
+
 public class SnakeCamelUtil {
 	
-	public static String snakeToCamelcase(String snake_case) {
+	public String snakeToCamelcase(String snake_case) {//staticだと実行できなかったためstaticをとった
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < words.length - 1; i++) {
@@ -11,18 +12,18 @@ public class SnakeCamelUtil {
 		return new String(sb);
 	}
 	 
-	public static String camelToSnakecase(String camelcase) {
+	public String camelToSnakecase(String camelcase) {//staticだと実行できなかったためstaticをとった
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
 			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
+			if (Character.isUpperCase(c)) {//大文字かどうかの判断
 				sb.append(camelcase.substring(j, i));
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
@@ -30,15 +31,15 @@ public class SnakeCamelUtil {
 	}
 	
 	static String capitalize(String s) {
-		char first = s.charAt(0);
-		char upperFirst = Character.toUpperCase(first);
-		String rest = s.substring(1);
+		char first = s.charAt(0); //charAt()は0を基点としてn番目の文字を返すメソッド
+		char upperFirst = Character.toUpperCase(first);//toUpperCaseは文字列に含まれるすべての文字を大文字に変換する。
+		String rest = s.substring(1);//subStringは文字列の指定した間の文字を取り出す。
 		return upperFirst + rest;
 	}
 
 	static String uncapitalize(String s) {
 		char first = s.charAt(0);
-		char lowerFirst = Character.toLowerCase(first);
+		char lowerFirst = Character.toLowerCase(first);//toUpperCaseは文字列に含まれるすべての文字を小文字に変換する。
 		String rest = s.substring(1);
 		return lowerFirst + rest;
 	}
--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,28 @@
 package snakecamel;
 
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+//import org.junit.runners.model.InitializationError;
 
-public class SnakeCamelTest {
+
+public class SnakeCamelTest extends SnakeCamelUtil{
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeToCamelcaseの実行結果(){//スネークケースからキャメルケースに変える
+		SnakeCamelUtil camel = new SnakeCamelUtil();
+		String expected = "SnakeCamel";
+	    String actual = camel.snakeToCamelcase("snake_camel_ ");
+	    assertThat(actual,is(expected));
 	}
-
+	
+	@Test
+	public void camelToSnakecaseの実行結果() {//キャメルケースからスネークケースに変える
+		SnakeCamelUtil camel = new SnakeCamelUtil();
+		String expected = "snake_camel";
+	    String actual = camel.camelToSnakecase("SnakeCamel");
+	    assertThat(actual,is(expected));
+	}
 }
